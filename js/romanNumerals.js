417c90a8dc4aefda56d6750b45fe25f6bfa2c14c
robertuptc <robertuptc@gmail.com>
2025-03-01 10:27:40 -0500

    lazy roman numerals finished

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,3 +1,24 @@
-exports.toRoman = function(num) {
+exports.toRoman = function(inputNum) {
+    // create an OUTPUT_STRING set to ""
+    let outputString = '';
+    let num = inputNum
 
+    // create an LAZY_ROMAN_NUMERAL object
+    let lazyRomanNumeral = {
+        'M' : 1000,
+        'D' : 500,
+        'C' : 100,
+        'L' : 50,
+        'X' : 10,
+        'V' : 5,
+        'I' : 1,
+    }
+    
+    for (let [key, value] of Object.entries(lazyRomanNumeral)) {
+            while (num >= value) {
+                outputString += key
+                num = num - value
+            }
+        }
+    return outputString
 };
