f35848e23b1787df164bd992f4de3a18e5baa4da
robertuptc <robertuptc@gmail.com>
2025-03-01 10:59:54 -0500

    js problem finished

--- js/romanNumerals.js
+++ js/romanNumerals.js
@@ -1,24 +1,37 @@
 exports.toRoman = function(inputNum) {
     // create an OUTPUT_STRING set to ""
-    let outputString = '';
+    let outputString = [];
     let num = inputNum
-
+    let smallerNumber = ''
     // create an LAZY_ROMAN_NUMERAL object
     let lazyRomanNumeral = {
         'M' : 1000,
+        'CM' : 900,
         'D' : 500,
+        "CD" : 400,
         'C' : 100,
+        "XC" : 90,
         'L' : 50,
+        "XL" : 40, 
         'X' : 10,
+        "IX" : 9,
         'V' : 5,
+        "IV" : 4,
         'I' : 1,
     }
     
     for (let [key, value] of Object.entries(lazyRomanNumeral)) {
+        if (num === value) {
+            outputString += key;
+            num = num - value
+        }
+        else {
             while (num >= value) {
                 outputString += key
                 num = num - value
+                }
             }
         }
+    console.log(outputString)
     return outputString
 };
--- js/romanNumeralsSpec.js
+++ js/romanNumeralsSpec.js
@@ -1,5 +1,6 @@
 var rn = require("./romanNumerals");
 
-console.log(rn.toRoman(1) === 'I');
-console.log(rn.toRoman(3) === 'III');
-console.log(rn.toRoman(4) === 'IV');
+// console.log(rn.toRoman(1) === 'I');
+// console.log(rn.toRoman(3) === 'III');
+// console.log(rn.toRoman(4) === 'IV');
+rn.toRoman(944)
